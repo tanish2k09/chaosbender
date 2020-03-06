400c724c4161a50b9b9d6f0460a52399f2cc884f
Tanish Manku <tanish2k09.dev@gmail.com>
2020-03-06 15:41:30 -0800

    Introduce DatabaseHandler class
    Signed-off-by: Tanish Manku <tanish2k09.dev@gmail.com>

---
+++ chaosbender/Handlers/DatabaseHandler.cs
@@ -0,0 +1,18 @@
+﻿using Chaosbender.Helpers.Security;
+using MongoDB.Driver;
+using System;
+using System.Threading.Tasks;
+
+namespace Chaosbender.Handlers.Data
+{
+  class DatabaseHandler
+  {
+    private static MongoClient dbClient = new MongoClient(CredentialsKeeper.MongoConnection);
+
+    public static async Task InitDB()
+    {
+      var cursor = await dbClient.ListDatabasesAsync();
+      await cursor.ForEachAsync(db => Console.WriteLine(db["name"]));
+    }
+  }
+}
