aa02a321277188616218d0113c7f9b2b97b74fd7
Tanish Manku <tanish2k09.dev@gmail.com>
2020-03-06 15:40:22 -0800

    Update CommandStore logic
    Signed-off-by: Tanish Manku <tanish2k09.dev@gmail.com>

--- chaosbender/Helpers/Commands/CommandStore.cs
+++ chaosbender/Helpers/Commands/CommandStore.cs
@@ -7,30 +7,34 @@ namespace Chaosbender.Helpers.Commands
   class CommandStore
   {
     // TODO: Aliases are server-specific. A single static data point won't scale properly. Make them specific.
-    private static Hashtable Aliases = new Hashtable() 
-    {
-    };
+    private static Hashtable Aliases = new Hashtable();
 
     // Maintain a duplicate hashtable for commands, purely for O(1) search
     // The value of key isn't of significance in this table
+    // TODO: Convert this Hashtable to a database table
     private static Hashtable Commands = new Hashtable()
     {
       {"alias", "alias" },
       {"ban", "ban" }
     };
 
+    /* GetActualCommand logic:
+     *
+     * Commands might be aliases, a direct command, or an unidentified trigger
+     * If it's an alias, find the direct command for it
+     * If it's not an alias, return the trigger as-is
+     * It is CommandHandler's responsibility to filter unidentified triggers in switches
+     * CommandHandler should also handle reading the custom commands for appropriate perm level
+     * from the database
+     */
     public static string GetActualCommand(string command)
     {
-      if (Commands.ContainsKey(command))
-      {
-        return command;
-      }
-      else if (Aliases.ContainsKey(command)) 
+      if (Aliases.ContainsKey(command)) // TODO: CRUD from db
       {
         return (string) Aliases[command];
       }
 
-      return null;
+      return command;
     }
 
     public static async Task<string> Link(string alias, string command)
